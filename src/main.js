6ac9be4066caeb68ff0b3a349951d9226d26347a
kthss01 <kthss01@naver.com>
2026-02-05 11:32:05 +0900

    Resolve combat MVP scene conflict

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import GameScene from './scenes/GameScene.js';
+import CombatMVPScene from './scenes/CombatMVPScene.js';
 import UILayoutScene from './scenes/UILayoutScene.js';
 
 const config = {
@@ -7,7 +7,7 @@ const config = {
   height: 540,
   backgroundColor: '#0f172a',
   parent: 'game-root',
-  scene: [UILayoutScene, GameScene],
+  scene: [UILayoutScene, CombatMVPScene],
 };
 
 new Phaser.Game(config);
